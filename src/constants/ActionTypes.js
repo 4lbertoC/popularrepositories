3dc7f226d2f51b2f892d366169a553eaa99aaf51
Alberto Congiu <4lbertoC@gmail.com>
2014-12-26 12:29:47 +0100

    added a note in the comments of original files that have been modified

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -4,6 +4,8 @@
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE.txt file in the root directory of this source tree.
+ *
+ * Modified by @4lbertoC
  */
 
 module.exports = {
